01d93ab13e4a532d936fdfc4d3b144b511d70404
Viktor <partykav2017@gmail.com>
2021-08-17 00:40:45 +0300

    homework-20 added

---
+++ src/components/jsx/Posting.jsx
@@ -0,0 +1,29 @@
+import Post from './Post';
+import Anakin from './../img/Anakin.jpg';
+import postImage from './../img/image_post.jpg';
+
+const ANAKIN_IMAGE = Anakin;
+const RAY_IMAGE = postImage;
+
+function Posting() {
+	return (
+		<div className="App">
+			<div className="container"></div>
+			<Post
+				author={{
+					name: 'Anakin Skywalker',
+					avatar: ANAKIN_IMAGE,
+					nickname: '@dart_vader',
+					date: 'Jul 31',
+					text: 'WTF? Who is Ray? Why she is Skywalker? Luke...? Fuck...',
+					photo: RAY_IMAGE,
+				}}
+				amountComment="350"
+				amountRetweet="124"
+				amountLike="2350"
+			/>
+		</div>
+	);
+}
+
+export default Posting;
